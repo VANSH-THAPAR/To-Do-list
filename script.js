1a724ef6109acf5503af1fefcb58d661e5dbc06c
VANSH-THAPAR <vanshthapar.professional@gmail.com>
2024-11-15 01:26:40 +0530

    updated

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function updatetime(){
 };
 setInterval(updatetime,1000)
 function remove() {
-    localStorage.delete(num)
+    localStorage.removeItem(num)
 }
 add.addEventListener('click', () => {
     num++
@@ -118,50 +118,51 @@ add.addEventListener('click', () => {
 
 inpval.addEventListener('keydown', (event) => {
     if(event.key=="Enter"){
-        num++
-        var li = document.createElement('li')
-        localStorage.setItem(1, num)
-        localStorage.setItem(num, inpval.value)
-        var removebutton = document.createElement('div')
-        removebutton.textContent = 'x'
-        li.innerHTML = localStorage.getItem(num)
-        removebutton.style.borderRadius ='50%';
-        removebutton.style.width ='20px';
-        removebutton.style.cursor ='pointer'
-        removebutton.style.height ='20px';
-        removebutton.style.justifyContent='center'
-        removebutton.style.alignItems='center'
-        removebutton.style.display='flex';
-        removebutton.style.border='2px solid white'
-        li.appendChild(removebutton)
-        li.style.listStyle = 'none'
-        li.style.paddingLeft = '10px'
-        li.style.paddingBottom = '4px'
-        li.style.fontWeight='bold'
-        removebutton.style.fontfamily='Segoe UI'
-        removebutton.style.color='blace';
-        li.style.color='black';
-        li.style.fontSize='20px';
-        li.style.fontSize='20px';
-        list.appendChild(li)
-        inpval.value = ''
-        let z='';
-        removebutton.addEventListener('click', (value) => {
-            value.target=z
-            for(let i=2;i<localStorage.length;i++){
-                if(z==localStorage[i]){
-                    localStorage.removeItem(i)
-                }
-            }
-            removebutton.parentElement.style.display = 'none';
-            localStorage.removeItem(num)
+    add.click();
+        //     num++
+    //     var li = document.createElement('li')
+    //     localStorage.setItem(1, num)
+    //     localStorage.setItem(num, inpval.value)
+    //     var removebutton = document.createElement('div')
+    //     removebutton.textContent = 'x'
+    //     li.innerHTML = localStorage.getItem(num)
+    //     removebutton.style.borderRadius ='50%';
+    //     removebutton.style.width ='20px';
+    //     removebutton.style.cursor ='pointer'
+    //     removebutton.style.height ='20px';
+    //     removebutton.style.justifyContent='center'
+    //     removebutton.style.alignItems='center'
+    //     removebutton.style.display='flex';
+    //     removebutton.style.border='2px solid white'
+    //     li.appendChild(removebutton)
+    //     li.style.listStyle = 'none'
+    //     li.style.paddingLeft = '10px'
+    //     li.style.paddingBottom = '4px'
+    //     li.style.fontWeight='bold'
+    //     removebutton.style.fontfamily='Segoe UI'
+    //     removebutton.style.color='blace';
+    //     li.style.color='black';
+    //     li.style.fontSize='20px';
+    //     li.style.fontSize='20px';
+    //     list.appendChild(li)
+    //     inpval.value = ''
+    //     let z='';
+    //     removebutton.addEventListener('click', (value) => {
+    //         value.target=z
+    //         for(let i=2;i<localStorage.length;i++){
+    //             if(z==localStorage[i]){
+    //                 localStorage.removeItem(i)
+    //             }
+    //         }
+    //         removebutton.parentElement.style.display = 'none';
+    //         localStorage.removeItem(num)
             
-            num--
-            localStorage.setItem(1, num)
-        })
-        li.style.width='90%'
-        li.style.display='flex'
-        li.style.justifyContent='space-between'    
+    //         num--
+    //         localStorage.setItem(1, num)
+    //     })
+    //     li.style.width='90%'
+    //     li.style.display='flex'
+    //     li.style.justifyContent='space-between'    
     }
 })
 clearall.addEventListener('click', () => {
